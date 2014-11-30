6124583a4b5be0fff97ce78f944f03e4ed1d620b
Costas Basdekis <costas.basdekis@gmail.com>
2014-11-30 09:16:18 +0000

    Test wall behind bigger wall

--- tests.hs
+++ tests.hs
@@ -14,8 +14,19 @@ testList = TestList $ map TestCase [
         ,assertShadows "No walls produce no walls"
             (Point 0 0) []
             []
-        ,assertShadows "One wall produces one wall"
+        ,assertShadows "One wall produces same wall"
             (Point 0 0) [(Wall (Point (-1) 1) (Point 1 1))]
+            [(Wall (Point (-1) 1) (Point 1 1))]
+        ,assertShadows "Two non-overlapping walls produce same walls"
+            (Point 0 0)
+            [(Wall (Point (-1) 1) (Point 1 1)),
+             (Wall (Point (-1) (-1)) (Point 1 (-1)))]
+            [(Wall (Point (-1) 1) (Point 1 1)),
+             (Wall (Point (-1) (-1)) (Point 1 (-1)))]
+        ,assertShadows "A wall behind a bigger wall is hidden"
+            (Point 0 0)
+            [(Wall (Point (-1) 1) (Point 1 1)),
+             (Wall (Point (-1) 2) (Point 1 2))]
             [(Wall (Point (-1) 1) (Point 1 1))]
     ]
 
