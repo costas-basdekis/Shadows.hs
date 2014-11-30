e7c5c1a3e064e76ddcab15e3757e4aa841644517
Costas Basdekis <costas.basdekis@gmail.com>
2014-11-30 09:23:52 +0000

    Test was too big, try testing wall to shadow

--- Shadows.hs
+++ Shadows.hs
@@ -2,6 +2,11 @@ module Shadows where
 
 data Point = Point Float Float deriving (Eq, Show)
 data Wall = Wall Point Point deriving (Eq, Show)
+data Shoint = Shoint Float Float deriving (Eq, Show)
+data Shadow = Shadow Shoint Shoint deriving (Eq, Show)
 
 createShadows :: Point -> [Wall] -> [Wall]
 createShadows _ walls = walls
+
+wallToShadow :: Point -> Wall -> Shadow
+wallToShadow = undefined
--- tests.hs
+++ tests.hs
@@ -4,9 +4,15 @@ import Shadows
 import Test.HUnit
 
 
+-- Test the input->expected of `createShadows`
 assertShadows testName light walls expected =
     assertEqual testName expected $
         createShadows light walls
+
+-- Test the input->expected of `wallToShadow`
+assertWallToShadow testName light wall expected =
+    assertEqual testName expected $
+        wallToShadow light wall
 
 testList = TestList $ map TestCase [
         assertEqual "The test suite runs"
@@ -23,11 +29,17 @@ testList = TestList $ map TestCase [
              (Wall (Point (-1) (-1)) (Point 1 (-1)))]
             [(Wall (Point (-1) 1) (Point 1 1)),
              (Wall (Point (-1) (-1)) (Point 1 (-1)))]
+        {- This test is too big, we need to break it down
         ,assertShadows "A wall behind a bigger wall is hidden"
             (Point 0 0)
             [(Wall (Point (-1) 1) (Point 1 1)),
              (Wall (Point (-1) 2) (Point 1 2))]
             [(Wall (Point (-1) 1) (Point 1 1))]
+        -}
+        ,assertWallToShadow "Simple wall to shadow"
+            (Point 0 0)
+            (Wall (Point 1 0) (Point 0 1))
+            (Shadow (Shoint 0 1) (Shoint (pi/4) 1))
     ]
 
 main = runTestTT testList
