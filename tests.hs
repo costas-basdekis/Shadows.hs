da4559926763b9d6e478f07799a640a11eea7303
Costas Basdekis <costas.basdekis@gmail.com>
2014-11-30 09:12:42 +0000

    Refactor tests with helper

--- tests.hs
+++ tests.hs
@@ -4,15 +4,19 @@ import Shadows
 import Test.HUnit
 
 
+assertShadows testName light walls expected =
+    assertEqual testName expected $
+        createShadows light walls
+
 testList = TestList $ map TestCase [
         assertEqual "The test suite runs"
             True True
-        ,assertEqual "No walls produce no walls"
+        ,assertShadows "No walls produce no walls"
+            (Point 0 0) []
             []
-            (createShadows (Point 0 0) [])
-        ,assertEqual "One wall produces one wall"
+        ,assertShadows "One wall produces one wall"
+            (Point 0 0) [(Wall (Point (-1) 1) (Point 1 1))]
             [(Wall (Point (-1) 1) (Point 1 1))]
-            (createShadows (Point 0 0) [(Wall (Point (-1) 1) (Point 1 1))])
     ]
 
 main = runTestTT testList
